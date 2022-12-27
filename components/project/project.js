c5b9894730ae2c1176235994a22fd897f77c0920
Pawel Rybka <rrybka1999@gmail.com>
2022-12-27 17:36:46 +0100

    Project section completed

--- components/project/project.js
+++ components/project/project.js
@@ -9,6 +9,7 @@ const project = () => {
     <div className={styles.project}>
         <p className={styles.project__title}>Projects</p>
         <div className={styles.project__section}>
+            
             <div className={styles.project__section__first}>
                 <img src='bird.png' className={styles.project__section__picture}/>
                 <p className={styles.project__section__des}>Flappy bird clone</p>
@@ -21,19 +22,23 @@ const project = () => {
                     <button>View</button>
                     <button>Code</button>
                 </div>
-            
-            
-            
             </div>
-            <div className={styles.project__section__second}>
-
-                
+        
+            <div className={styles.project__section__first}>
+                <img src='calculator.png' className={styles.project__section__picture}/>
+                <p className={styles.project__section__des}>iOS calculator clone</p>
+                <div className={styles.project__section__icons}>
+                    <AiFillHtml5 size={60}/>
+                    <DiCss3 size={60}/>
+                    <DiJavascript1 size={60}/>
+                </div>
+                <div className={styles.project__section__buttons}>
+                    <button>View</button>
+                    <button>Code</button>
+                </div>
             </div>
-            
-
+        
         </div>
-    
-    
     </div>
   )
 }
