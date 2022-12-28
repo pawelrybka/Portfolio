3388278d08f5028bfead5c919a3bb9543e0b211f
Pawel Rybka <rrybka1999@gmail.com>
2022-12-28 14:20:36 +0100

    Contact section completed

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -6,6 +6,11 @@ const contact = () => {
   return (
     <div className={styles.contact}>
         <p className={styles.contact__title}>Contact with me!</p>
+        <form>
+          <input type='text' placeholder="Your email" required></input>
+          <textarea type="text" name="message" placeholder="Your message" required/>            
+          <button type="submit" class="submit">Send</button>
+        </form>
     </div>
   )
 }
