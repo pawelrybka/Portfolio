2e47ac57384826a044d6996a946efcf0e28308f4
Pawel Rybka <rrybka1999@gmail.com>
2023-01-22 13:32:35 +0100

    Add meta tag to the _app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import '../src/index.css';
 import type { AppProps } from 'next/app'
 import Navbar from '@/src/components/navbar/navbar';
 import Footer from '@/src/components/footer/footer';
+import Head from 'next/head';
 
 function App ({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <Navbar/>
       <Component {...pageProps} />
       <div className='color-layout layout-blue'></div>
@@ -17,3 +21,4 @@ function App ({ Component, pageProps }: AppProps) {
 }
 
 export default App
+
